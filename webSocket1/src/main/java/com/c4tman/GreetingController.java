53dd11bead89606ac764a009bdf5150452ff4f25
C4TMAN <xiaoman.zhang@changhong.com>
2017-01-13 09:55:07 +0800

    add annotation

--- webSocket1/src/main/java/com/c4tman/GreetingController.java
+++ webSocket1/src/main/java/com/c4tman/GreetingController.java
@@ -14,9 +14,9 @@ public class GreetingController {
     @Autowired
     private SimpMessageSendingOperations simpMessageSendingOperations;
 
-    @MessageMapping("/pushMessaging")
+    @MessageMapping("/pushMessaging")//服务端接收消息地址（实际是/app/pushMessaging）
     public void messaging(Messagings messagings) throws Exception {
         simpMessageSendingOperations.convertAndSendToUser(messagings.getId(), "/topic/messagings",
-                messagings.getContent());
+                messagings.getContent());//向客户端推送消息（用户id， 订阅地址，消息内容）
     }
 }
